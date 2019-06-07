b089b6bd18139b86f29837b4b181d4268b310aaf
hetzz <hetal.kuvadia@somaiya.edu>
2019-06-07 19:17:31 +0530

    Added python files for 3 problems

--- Assessment/First10.java
+++ Assessment/First10.java
@@ -1,15 +1,33 @@
 import java.util.*;
 class First10{
+    public static Boolean iscalculateExpression(int a ,int b,int c){
+        a = math.pow(a,3);
+        b = math.pow(b,3);
+        c = math.pow(c,2); 
+        return a + b == c;        
+    }
     public static Vector generateTriples(){
         Vector triple = new Vector();
         a = 1;
         b = 2;
         c = 3;
         while(triple.size() < 10){
-            if (iscalculateExpression(a,b,c))
+            if (iscalculateExpression(a,b,c)){
+                Vector tripl = new Vector ();
+                triple.add(tripl);
+
+            }
+            a += 1;
+            b += 1;
+            c += 1;
         }
+        return triple;
     }
     public static void main(String[] args){
-        System.out.println(tripl);
+        Vector triple = new Vector();
+        triple = generateTriples();
+        Enumeration vEnum = triple.elements();
+        while(vEnum.hasMoreElements())
+            System.out.print(vEnum.nextElement() + " ");
     }
 }
