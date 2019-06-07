677eb51051cfaf1637f90ddd868ed4cce396fd5f
hetzz <hetal.kuvadia@somaiya.edu>
2019-06-07 19:07:31 +0530

    Added python files for 3 problems

--- Assessment/First10.py
+++ Assessment/First10.py
@@ -1,25 +1,29 @@
 import math
 
-def iscalculateExpression(a, b,c):
+def iscalculateExpression(a, b, c):
     a = math.pow(a,3)
     b = math.pow(b,3)
-    c = math.pow(c,2)
+    c = math.pow(c,2) 
     return a + b == c
 
 #############################################################
 
 def generateTriples(num_of_literals):
     triples = []
-    a = 0
-    b = 0
-    c = 0
-    while (len(triples) != num_of_literals) :
+    a = 1
+    b = 2
+    c = 3
+    while (len(triples) < num_of_literals) :
         if (iscalculateExpression(a,b,c)) :
             tripl = [a ,b, c]
             triples.append(tripl)
-        else :
-            a += 1
-            b += 1
+        
+        a += 1
+        b += 1
+        c += 1
+    
+    return triples
 
 #######################################################
 num_of_literals = int(input())
+print(generateTriples(num_of_literals))
