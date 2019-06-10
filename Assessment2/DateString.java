66701d7482fcdae06a16a5b555bd379a702e03c4
hetzz <hetal.kuvadia@somaiya.edu>
2019-06-10 17:07:18 +0530

    Added Java files

--- Assessment2/DateString.java
+++ Assessment2/DateString.java
@@ -1,10 +1,19 @@
 import java.util.*;
 class DateString{
+    public static Boolean isAmbiguous(int firstParam, int secondParam){
+        return (firstParam <= 12 && secondParam <= 12);
+    }
+
+    public static Boolean dateAmbiguity(String dateString){
+        String [] date = dateString.split("/");
+        return (isAmbiguous(Integer.parseInt(date[0]),Integer.parseInt(date[1])));
+
+    }
     public static void main(String[] args){
         
         Scanner sc = new Scanner(System.in);
-        String  date = sc.nextLine();
-        
+        String  dateString = sc.nextLine();
+        System.out.println("The fact that the string is ambiguous is :" +dateAmbiguity(dateString));
 
     }
 }
