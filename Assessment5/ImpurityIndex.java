72df2326d0bb287ce121be8ddccbf11e05e2249f
hetzz <hetal.kuvadia@somaiya.edu>
2019-06-18 10:34:22 +0530

    Added Assignment 5

--- Assessment5/ImpurityIndex.java
+++ Assessment5/ImpurityIndex.java
@@ -16,15 +16,15 @@ class Impurity{
 
     public static int impurity_count(String sentence){
         int impurity_index = 0;
-        int [] letter_array = Letter_occurence_computation(sentence);
+        int [] letter_array = letter_occurence_computation(sentence);
         char [] vowels = {'a','e','i','o','u'};
-        int [] impurity_increment = {0, 0.5, 0.7 ,1, 3};
+        double [] impurity_increment = {0, 0.5, 0.7 ,1, 3};
         for (int i = 0; i < 26; i++){
-            if(new String(vowels).indexOf(chr(i + 97)) != -1){
+            if(new String(vowels).indexOf((char)(i + 97)) != -1){
                 if(letter_array[i] == 2) 
                     impurity_index += impurity_increment[1];
             }
-            if(new String(vowels).indexOf(chr(i + 97)) == -1 && letter_array[i] == 2)
+            if(new String(vowels).indexOf((char)(i + 97)) == -1 && letter_array[i] == 2)
                 impurity_index += impurity_increment[2];
             if(letter_array[i] == 3)
                 impurity_index += impurity_increment[3];
@@ -34,7 +34,7 @@ class Impurity{
         return (impurity_index);
     }
     public static void main(String args[]){
-        String s = "Old brother fox jumps over the lazy dog.";
+        String sentence = "Old brother fox jumps over the lazy dog.";
         System.out.println(impurity_count(sentence));
     }
 }
